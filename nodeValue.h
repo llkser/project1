5b9c5fb122cc7a79d943f73e930af5d7739dbd24
llkser <45782035+llkser@users.noreply.github.com>
2018-12-14 01:20:10 +0800

    Add files via upload

---
+++ nodeValue.h
@@ -0,0 +1,8 @@
+#include "treeStructure.h"
+//functions that change the tree
+
+double nodeValue( Node *node, double time );
+double value( double x, double y, double time );
+void SetnodeValue( Node *head );
+void changeTree( Node *head );
+void adapt( Node *head );
