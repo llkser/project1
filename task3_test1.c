1517f0f329bea35f238c5a91d4b2ccb0f5e089b2
llkser <45782035+llkser@users.noreply.github.com>
2018-12-14 01:28:16 +0800

    Add files via upload

---
+++ task3_test1.c
@@ -0,0 +1,38 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+#include "buildTree.h"
+#include "writeTree.h"
+#include "treeStructure.h"
+#include "nodeValue.h"
+// count of how many nodes are added and how many are removed during task3_stage 2
+int nodeAdded,nodeRemoved;
+// main
+
+int main( int argc, char **argv ) {
+
+  Node *head;
+
+  // make the head node
+  head = makeNode( 0.0,0.0, 0 );
+  nodeAdded=nodeRemoved=0;
+
+  // make a tree
+  growTree(head);
+  growTree(head);
+  growTree(head);
+  writeTree( head );
+  SetnodeValue(head);
+  changeTree(head);
+  printf("Nodes added: %d\n",nodeAdded);
+  printf("Nodes removed: %d\n",nodeRemoved);
+  SetnodeValue(head);
+
+  // print the tree for Gnuplot
+  writeTree( head );
+
+  //free the whole tree
+  destroyTree(head);
+
+  return 0;
+}
